ff06ab7a62c3e8208fd6c866711996f7470f9690
Andreea15B <andreea.bucataru15@yahoo.ro>
2020-04-25 14:52:31 +0300

    Added draw case

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -40,43 +40,6 @@ export class GithubService {
         })
       );
   }
-
-  // getUserOne(username) {
-  //   this.isLoading1$.next(true);
-  //   this.http.get(`https://api.github.com/users/${username}`)
-  //   .pipe(
-  //     delay(500),
-  //     tap(() => this.isLoading1$.next(false))
-  //   )
-  //   .subscribe((res) => {
-  //       console.log("Success for user1: ", res);
-  //       this.playerOneInfo.next(res);
-  //     },
-  //     (err) => {
-  //       console.log("Error in user1 service: ", err)
-  //       this.playerOneInfo.next(err);
-  //     }
-  //   );
-  // }
-
-  // getUserTwo(username) {
-  //   this.isLoading2$.next(true);
-  //   this.http.get(`https://api.github.com/users/${username}`)
-  //   .pipe(
-  //     delay(500),
-  //     tap(() => this.isLoading2$.next(false))
-  //   )
-  //   .subscribe(
-  //     (res) => {
-  //       console.log("Success for user2: ", res);
-  //       this.playerTwoInfo.next(res);
-  //     },
-  //     (err) => {
-  //       console.log("Error in user2 service: ", err)
-  //       this.playerTwoInfo.next(err);
-  //     }
-  //   )
-  // }
   
 }
 
